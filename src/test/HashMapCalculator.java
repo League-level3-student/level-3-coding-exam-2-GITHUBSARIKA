25e33b443a91cfac346dd2e65223534d98dff79b
League Student <league@iMac-22.attlocal.net>
2019-10-03 18:13:23 -0700

    z

--- src/test/HashMapCalculator.java
+++ src/test/HashMapCalculator.java
@@ -5,11 +5,20 @@ import java.util.HashMap;
 
 public class HashMapCalculator {
 ArrayList<String> commonKeyValues(ArrayList<Integer> lst, HashMap<Integer, String> hm){
-	for (int i = 0; i < lst.size(); i++) {
-		
+	ArrayList<String> commonValues=new ArrayList<String>();
+	
+		for (int i = 0; i < lst.size(); i++) {
+	  String x=hm.get(lst.get(i));
+	  if(x!=null) {
+		  commonValues.add(x);
+		  
+	  }
 	}
-	for (int i = 0; i < hm.size(); i++) {
-		
-	}
+	
+	
+	return commonValues;
+	
+	
+	
 }
 }
